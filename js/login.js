26170378a4303c8062976e25d1244ed9b0001460
Tatiane Entler <94848789+tatientler@users.noreply.github.com>
2022-03-23 10:45:42 -0300

    Correção de classe no JS

--- js/login.js
+++ js/login.js
@@ -1,11 +1,11 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
+const registrarBotao = document.getElementById('registrar');
+const entrarBotao = document.getElementById('entrar');
 const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () => {
+registrarBotao.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener('click', () => {
+entrarBotao.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
