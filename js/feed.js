521767f63040651443938feacd95c4c6b7491643
Alysson <alyssonbarrera@outlook.com>
2022-04-09 12:25:33 -0300

    ajustes em animação e posicionamento

--- js/feed.js
+++ js/feed.js
@@ -1,17 +1,13 @@
 let btnChat = document.querySelector('.btnChat')
-let animationDataChat = document.querySelector('[data-anime-chat]')
-let chatContainer = document.querySelector('.container')
-let chatBox = document.querySelector('.chatBox')
 let chat = document.querySelector('.chat')
 let iconChat = document.querySelector('#btnChat')
 
 // Função que oculta chat
 function collapseChat () {
-    if(chatContainer.classList.contains('animate') && chatBox.classList.contains('animate') && iconChat.classList.contains('msg-icone-g')){
-        chatContainer.classList.remove('animate');
-        chatBox.classList.remove('animate');
+    if(chat.classList.contains('animate') && iconChat.classList.contains('msg-icone-g')){
+        chat.classList.remove('animate');
         iconChat.classList.remove('msg-icone-g');
         iconChat.classList.add('msg-icone')        
-    } else{chatContainer.classList.add('animate'); chatBox.classList.add('animate'); iconChat.classList.remove('msg-icone'); iconChat.classList.add('msg-icone-g')}
+    } else{chat.classList.add('animate'); iconChat.classList.remove('msg-icone'); iconChat.classList.add('msg-icone-g')}
 }
 btnChat.addEventListener('click', collapseChat)
