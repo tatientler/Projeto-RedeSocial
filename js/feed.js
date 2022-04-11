3a08e3216dd0fad6e18e70210fc13beb50746e67
Alysson <alyssonbarrera@outlook.com>
2022-04-10 22:11:21 -0300

    update chat

--- js/feed.js
+++ js/feed.js
@@ -2,8 +2,14 @@ let btnChat = document.querySelector('.btnChat')
 let chat = document.querySelector('.chat')
 let iconChat = document.querySelector('#btnChat')
 
+let inputMensagem = document.querySelector('#mensagem')
+let btnMessagem = document.querySelector('#btnMessagem')
+let containerChat = document.querySelector('.container')
+let divChat = document.querySelector('.divChat')
+
 // Função que oculta chat
 function collapseChat () {
+    divChat.scrollTop = divChat.scrollHeight * 100 // Para colocar o scroll no fim da div ao clicar em btnChat
     if(chat.classList.contains('animate') && iconChat.classList.contains('msg-icone-g')){
         chat.classList.remove('animate');
         iconChat.classList.remove('msg-icone-g');
@@ -11,3 +17,41 @@ function collapseChat () {
     } else{chat.classList.add('animate'); iconChat.classList.remove('msg-icone'); iconChat.classList.add('msg-icone-g')}
 }
 btnChat.addEventListener('click', collapseChat)
+
+// Programação chat
+let mensagens = {
+        userName: 'John Doe',
+        userMessages: []
+    }
+
+function exibirMensagens () {
+    for (let i = 0; i < mensagens.userMessages.length; i++) {
+        containerChat.innerHTML += `<div class="msg"> <img src="imagens/chester-wade-msnyz9L6gs4-unsplash.jpg" alt="Avatar"> <p>${mensagens.userMessages[i]}</p> <span class="time-right">11:20</span> </div>`
+        divChat.scrollTop = divChat.scrollHeight // Para colocar o scroll na última mensagem adicionada
+    }
+}
+
+exibirMensagens()
+
+function newMessage () {
+    if(inputMensagem.value.match(/^(\s)+$/)) { // Para impedir envio de mensagens que contenham apenas espaço
+        return false
+    } else if (inputMensagem.value != "") {
+        mensagens.userMessages.push(inputMensagem.value); // Para adicionar a nova mensagem ao array
+        inputMensagem.value = ""; // Para limpar o input ao adicionar a mensagem ao array
+        inputMensagem.focus() // Para manter o foco no input mesmo após o envio de uma mensagem
+    } else {inputMensagem.value = ""}
+
+    while(containerChat.firstChild) { // Para evitar clones do conteúdo do array ao enviar a mensagem
+        containerChat.removeChild(containerChat.firstChild)
+    }
+    exibirMensagens()
+}
+
+inputMensagem.addEventListener('keypress', function(e) { // Para enviar mensagem com enter
+    if(e.which == 13) {
+        btnMessagem.click()
+    }
+})
+
+btnMessagem.addEventListener('click', newMessage)
