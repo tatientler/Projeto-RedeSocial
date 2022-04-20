a5a24830cd2b3914d0cd1b34281cee2f4fb24af6
Alysson <alyssonbarrera@outlook.com>
2022-04-20 13:03:52 -0300

    ajustes gerais

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,23 @@
 let publicar = document.getElementById('btnPublicar')
+let btnClose = document.querySelector('.btn-close')
+let input = document.getElementById('bodyModal')
 
 publicar.addEventListener('click', addPost)
+publicar.addEventListener('click', function() {
+    btnClose.click()
+    input.value = ''
+})
 
 function addPost(){
-    let input = document.getElementById('bodyModal')
+
     let valor = input.value
-    let nome = "Nome usuário"
+    let nome = "John Doe"
 
     let post = document.getElementById('posts')
 
     let teste = document.createElement('div')
     teste.setAttribute('class', 'post1')
-    teste.innerHTML = "<div class='author'><img src='./imagens/img3.jpg' alt=''><p>" +
+    teste.innerHTML = "<div class='author'><img src='imagens/chester-wade-msnyz9L6gs4-unsplash.jpg' alt='Avatar''><p>" +
     nome + "</p></div><div class='contentPost1'><p>" + valor + "</p></div><div class='interacoes container'><button class='btnInt'><i class='material-icons icone-interacoes'>thumb_up</i>Reagir</button><button class='btnInt'><i class='material-icons icone-interacoes'>comment</i>Comentar</button><button class='btnInt'><i class='material-icons icone-interacoes'>send</i>Compartilhar</button></div><div class='btnInteracoes d-flex align-items-center'><button class='btnAction'><i class='material-icons icone-interacoes'>thumb_up</i></button><button class='btnAction'><i class='material-icons icone-interacoes'>comment</i></button><button class='btnAction'><i class='material-icons icone-interacoes'>send</i></button></div>"
 
 
